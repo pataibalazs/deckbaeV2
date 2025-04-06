0c65ca748ed0d57a1eac7a6ea67426da51a513b1
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2025-04-06 13:45:11 +0200

    added sep

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.deckbae.hu",
+          },
+        ],
+        destination: "https://deckbae.hu/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
