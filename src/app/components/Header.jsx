4455ab8181ef20849504d43d84ec6a940fe20057
pataibalazs <79509500+pataibalazs@users.noreply.github.com>
2025-06-16 17:46:38 +0200

    fix

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -241,7 +241,7 @@ export default function Header() {
                   Garázsok
                 </Link>
                 <Link
-                  href="/garages"
+                  href="/services"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   onClick={() => setMobileMenuOpen(false)}
                 >
